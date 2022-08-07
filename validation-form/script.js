80c1cf8d1b46986899fe3d1816f2ed9facc1d578
Rostyslav <bovonenko@icloud.com>
2022-08-07 13:54:12 +0200

    added easiest validation to script.js

--- validation-form/script.js
+++ validation-form/script.js
@@ -4,7 +4,7 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-//Show input error message
+// Show input error message
 function showError(input, message) {
     const formControl = input.parentElement;
     formControl.className = 'form-control error'
@@ -13,7 +13,12 @@ function showError(input, message) {
     small.textContent = message;
 }
 
-//Show success outline
+// Check email is valid
+function isValidEmail(email) {
+    return !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email);
+}
+
+// Show success outline
 function showSuccess(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success'
@@ -27,5 +32,22 @@ form.addEventListener('submit', (e) => {
         showError(username, 'Username is required');
     } else {
         showSuccess(username);
+    }
+    if (email.value === '') {
+        showError(email, 'Email is required');
+    } else if(isValidEmail(email.value)) {
+        showError(email, 'Email is not valid');
+    } else {
+        showSuccess(email);
+    }
+    if (password.value === '') {
+        showError(password, 'Password is required');
+    } else {
+        showSuccess(password);
+    }
+    if (password2.value === '') {
+        showError(password2, 'Password is required');
+    } else {
+        showSuccess(password2);
     }
 })
